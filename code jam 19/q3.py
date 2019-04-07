9a582a03282aced555dc9bb351f88c36f29e7b71
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2019-04-07 09:45:45 +0530

    Add files via upload

--- code jam 19/q3.py
+++ code jam 19/q3.py
@@ -9,19 +9,18 @@ for h in range(no):
 
 	lar,leng=list(map(int,input().split()))
 	st=list(map(int,input().split()))
-	seed=gcd(st[0],st[1])
-	alph=set([])
-	alph.add(st[0]//seed)
-	alph.add(seed)
-	for i in range(1,leng):
-		alph.add(st[i]//seed)
-		seed=st[i]//seed
-	alph=list(alph)
+
+	let=[st[0]/gcd(st[0],st[1])]
+	for i in range(leng):
+		let+=[st[i]/let[-1]]
+
+	alph=[]
+	for i in let:
+	    if i not in alph:
+	        alph+=[i]
 	alph.sort()
 
-	final=chr(65+alph.index(st[0]/gcd(st[0],st[1])))
-	for i in range(leng-1):
-		final+=chr(65+alph.index(gcd(st[i],st[i+1])))
-	final+=chr(65+alph.index(st[-1]/gcd(st[-1],st[-2])))
-
+	final=''
+	for i in let:
+		final+=chr(alph.index(i)+65)
 	print('Case #'+str(h+1)+': '+final)
